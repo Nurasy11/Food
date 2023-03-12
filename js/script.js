41dcdffe0ccd16bab0a10f4c758e8714e2a81c27
Nurasyl <nurasylqazaqwws@gmail.com>
2023-03-12 14:28:41 +0600

    slider

--- js/script.js
+++ js/script.js
@@ -193,7 +193,10 @@ window.addEventListener('DOMContentLoaded', () => {
             data.forEach(({ img, altimg, title, descr, price }) => {
                 new MenuCards(img, altimg, title, descr, price, '.menu .container').render();
             })
-        })
+        });
+
+    axios.get('http://localhost:3000/menu')
+        .then(data => console.log(data))
 
     // getResource('http://localhost:3000/menu')
     //     .then(data => createCard(data));
@@ -333,4 +336,121 @@ window.addEventListener('DOMContentLoaded', () => {
         .then(data => data.json())
         .then(res => console.log(res));
 
+
+    //Slider
+    const slides = document.querySelectorAll('.offer__slide'),
+        prev = document.querySelector('.offer__slider-prev'),
+        next = document.querySelector('.offer__slider-next'),
+        total = document.querySelector('#total'),
+        current = document.querySelector('#current'),
+        slidesWrapper = document.querySelector('.offer__slider-wrapper'),
+        slidesField = document.querySelector('.offer__slider-inner'),
+        width = window.getComputedStyle(slidesWrapper).width;
+
+    let slideIndex = 1;
+    let offset = 0;
+
+    if (slides.length < 10) {
+        total.textContent = `0${slides.length}`;
+        current.textContent = `0${slideIndex}`
+    }
+    else {
+        total.textContent = slides.length;
+        current.textContent = slideIndex;
+    }
+
+    next.addEventListener('click', () => {
+        if (offset == +width.slice(0, width.length - 2) * (slides.length - 1)) {
+            offset = 0;
+        }
+        else {
+            offset += +width.slice(0, width.length - 2);
+        }
+        slidesField.style.transform = `translateX(-${offset}px)`
+        if (slideIndex == slides.length) {
+            slideIndex = 1;
+        }
+        else {
+            slideIndex++;
+        }
+
+        if (slides.length < 10) {
+            current.textContent = `0${slideIndex}`;
+        } else {
+            current.textContent = slideIndex;
+        }
+    });
+
+
+
+    
+    prev.addEventListener('click', () => {
+        if (offset == 0) {
+
+            offset = +width.slice(0, width.length - 2) * (slides.length - 1)
+        }
+        else {
+            offset -= +width.slice(0, width.length - 2);
+        }
+        slidesField.style.transform = `translateX(-${offset}px)`;
+
+        if (slideIndex == 1) {
+            slideIndex = slides.length;
+        }
+        else {
+            slideIndex--;
+        }
+
+        if (slides.length < 10) {
+            current.textContent = `0${slideIndex}`;
+        } else {
+            current.textContent = slideIndex;
+        }
+    });
+
+    slidesField.style.width = 100 * slides.length + '%';
+
+    slidesField.style.display = 'flex';
+    slidesField.style.transition = '0.5s all';
+
+    slidesWrapper.style.overflow = 'hidden';
+    slides.forEach(slide => {
+        slide.style.width = width;
+    })
+    // showSlides(slideIndex);
+    // if (slides.length < 10) {
+    //     total.textContent = `0${slides.length}`;
+    // }
+    // else {
+    //     total.textContent = slides.length;
+    // }
+    // function showSlides(n) {
+    //     if (n > slides.length) {
+    //         slideIndex = 1;
+    //     }
+    //     if (n < 1) {
+    //         slideIndex = slides.length;
+    //     }
+
+    //     slides.forEach(slide => slide.style.display = 'none');
+    //     slides[slideIndex - 1].style.display = 'block';
+
+
+    //     if (slides.length < 10) {
+    //         current.textContent = `0${slideIndex}`;
+    //     }
+    //     else {
+    //         current.textContent = slideIndex;
+    //     }
+    // }
+    // function plusSlide(n) {
+    //     showSlides(slideIndex += n);
+    // }
+
+    // prev.addEventListener('click', () => {
+    //     plusSlide(-1);
+    // })
+    // next.addEventListener('click', () => {
+    //     plusSlide(1);
+    // })
 });
