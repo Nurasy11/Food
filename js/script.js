172f86fd8bde7e65d914c9fd4900b908ecefbaa5
Nurasyl <nurasylqazaqwws@gmail.com>
2023-03-12 12:27:47 +0600

    adding axios

--- js/script.js
+++ js/script.js
@@ -187,13 +187,37 @@ window.addEventListener('DOMContentLoaded', () => {
         }
         return await res.json();
     }
-//новый канша обьект болсада кабылдай береді
+    //новый канша обьект болсада кабылдай береді
     getResource('http://localhost:3000/menu')
         .then(data => {
             data.forEach(({ img, altimg, title, descr, price }) => {
-                new MenuCards(img, altimg, title, descr, price).render();
+                new MenuCards(img, altimg, title, descr, price, '.menu .container').render();
             })
         })
+
+    // getResource('http://localhost:3000/menu')
+    //     .then(data => createCard(data));
+    // function createCard(data) {
+    //     data.forEach(({ img, altimg, title, descr, price }) => {
+    //         const element = document.createElement('div');
+
+
+    //         element.classList.add('menu__item');
+    //         element.innerHTML = `
+    //         <img src=${img} alt=${altimg}>
+    //         <h3 class="menu__item-subtitle">${title}</h3>
+    //         <div class="menu__item-descr">${descr}</div>
+    //         <div class="menu__item-divider"></div>
+    //         <div class="menu__item-price">
+    //             <div class="menu__item-cost">Цена:</div>
+    //             <div class="menu__item-total"><span>${price}</span> $/день</div>
+    //         </div>
+    //         `;
+    //         document.querySelector('.menu .container').append(element)
+    //     });
+    // }
+
+
     // new MenuCards(
     //     "img/tabs/vegy.jpg",
     //     "vegy",
