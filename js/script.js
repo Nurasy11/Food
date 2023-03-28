c501e3285d266adee824233c64602358530fd0ac
Nurasyl <nurasylqazaqwws@gmail.com>
2023-03-28 14:47:02 +0600

    modal settings

--- js/script.js
+++ js/script.js
@@ -5,16 +5,18 @@ import forms from './modules/forms';
 import modal from './modules/modal';
 import slider from './modules/slider';
 import timer from './modules/timer';
+import { openModal } from './modules/modal';
 
 
 window.addEventListener('DOMContentLoaded', () => {
 
+    const modalTimerId = setTimeout(() => openModal('.modal', modalTimerId), 30000000);
 
     tabs();
     calc();
     cards();
     forms();
-    modal();
+    modal('[data-modal]', '.modal', modalTimerId);
     slider();
     timer();
 
