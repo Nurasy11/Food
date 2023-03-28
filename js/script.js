c21b79d04bce80cbdd3ae8fca0e389eb790f865f
Nurasyl <nurasylqazaqwws@gmail.com>
2023-03-28 15:42:51 +0600

    timer

--- js/script.js
+++ js/script.js
@@ -6,19 +6,30 @@ import modal from './modules/modal';
 import slider from './modules/slider';
 import timer from './modules/timer';
 import { openModal } from './modules/modal';
+import { container } from 'webpack';
 
 
 window.addEventListener('DOMContentLoaded', () => {
 
     const modalTimerId = setTimeout(() => openModal('.modal', modalTimerId), 30000000);
 
-    tabs('.tabheader__item','.tabcontent','.tabheader__items','tabheader__item_active');
+    tabs('.tabheader__item', '.tabcontent', '.tabheader__items', 'tabheader__item_active');
     modal('[data-modal]', '.modal', modalTimerId);
     calc();
     cards();
-    slider();
+    slider({
+        container: '.offer__slider',
+        nextArrow: '.offer__slider-next',
+        prevArrow: '.offer__slider-prev',
+        slide: '.offer__slide',
+        totalCounter: '#total',
+        currentCounter: '#current',
+        wrapper: '.offer__slider-wrapper',
+        field: '.offer__slider-inner'
+
+    });
     forms('form', modalTimerId);
-    timer('.timer','2022-11-25');
+    timer('.timer', '2022-11-25');
 
 
 
